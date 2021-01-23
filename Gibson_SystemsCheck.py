c8904f35f0b393f95064986c8968f80c3bee72d7
dgibso1201 <57407971+dgibso1201@users.noreply.github.com>
2021-01-22 19:48:57 -0500

    Add files via upload

---
+++ Gibson_SystemsCheck.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+x = range(10) 
+
+
+# In[4]:
+
+
+y = []
+for i in x: 
+    y.append(i+3)
+
+
+# In[5]:
+
+
+print(x, y) 
+
+
+# In[6]:
+
+
+def subthree (x): 
+    return x-3 
+[subthree(i) for i in y] 
+
+
+# ## Heading
+# Jupyter is fun 
+# 
+# *italics* 
+# 
+# 
+
+# In[ ]:
+
+
+
+
